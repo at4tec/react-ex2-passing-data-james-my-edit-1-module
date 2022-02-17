55f83877e7b4b1f9654c8d5777f10a43a2f817d8
at4tec <at4tec@gmail.com>
2022-02-17 15:19:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ogkwww
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ogkwww)
---
+++ src/App.js
@@ -0,0 +1,157 @@
+import React,{Component} from "react";
+import "./style.css";
+
+
+const profiles = [
+  {
+    id: 1,
+    userID: "1",
+    favoriteMovieID: "1"
+  },
+  {
+    id: 2,
+    userID: "2",
+    favoriteMovieID: "1"
+  },
+  {
+    id: 3,
+    userID: "4",
+    favoriteMovieID: "5"
+  },
+  {
+    id: 4,
+    userID: "5",
+    favoriteMovieID: "2"
+  },
+  {
+    id: 5,
+    userID: "3",
+    favoriteMovieID: "5"
+  },
+  {
+    id: 6,
+    userID: "6",
+    favoriteMovieID: "4"
+  }
+];
+
+const users = {
+  1: {
+    id: 1,
+    name: "Jane Jones",
+    userName: "coder"
+  },
+  2: {
+    id: 2,
+    name: "Matthew Page",
+    userName: "mpage"
+  },
+  3: {
+    id: 3,
+    name: "Autumn Green",
+    userName: "user123"
+  },
+  4: {
+    id: 3,
+    name: "John Doe",
+    userName: "user123"
+  },
+  5: {
+    id: 5,
+    name: "Lauren Johnson",
+    userName: "user123"
+  },
+  6: {
+    id: 6,
+    name: "Nicholas Lain",
+    userName: "user123"
+  }
+};
+
+const movies = {
+  1: {
+    id: 1,
+    name: "Planet Earth 1"
+  },
+  2: {
+    id: 2,
+    name: "Selma"
+  },
+  3: {
+    id: 3,
+    name: "Million Dollar Baby"
+  },
+  4: {
+    id: 4,
+    name: "Forrest Gump"
+  },
+  5: {
+    id: 5,
+    name: "Get Out"
+  }
+};
+
+
+class App extends Component {
+  render() {
+    return (
+       
+        <>
+          <h2>How Popular is Your Favorite Movie?james</h2>
+          <PopularMovies 
+          profiles={profiles} 
+          users={users} 
+          movies={movies} />
+        </>
+    );
+  }
+}
+
+
+
+
+class PopularMovies extends Component {
+  render() {
+    const moviesArr = Object.values(movies);
+    return (
+      <div className="PopularMovies-container">
+        {moviesArr.map(movie => (
+          <div key={movie.id} className="PopularMovies-cell">
+            <h3>{movie.name}</h3>
+            <UserList movieID={movie.id} users={users} profiles={profiles} />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+
+
+class UserList extends Component {
+  render() {
+    const { movieID} = this.props;
+    const filteredProfiles = profiles.filter(
+      profile => Number(profile.favoriteMovieID) === movieID
+    );
+
+    // console.log(filteredProfiles);
+    if (!filteredProfiles || filteredProfiles.length === 0) {
+      return <p>None of the current users liked this movie</p>;
+    }
+
+    return (
+      <div>
+        <p>Liked by:</p>
+        <ul>
+          {filteredProfiles.map(profile => (
+            <li key={profile.userID}>{users[profile.userID].name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
+
